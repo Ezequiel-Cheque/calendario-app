ea2fb8133410dd839c7139e71207881acf6c1766
Ezequiel <ezekiel@platimex.com.mx>
2021-10-08 18:40:29 -0500

    building modal

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../ui/Navbar';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -6,17 +6,44 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { messages } from '../../helpers/calendar-messages-es';
 
 import 'moment/locale/es'
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
 moment.locale('es');
 
 const localizer = momentLocalizer(moment)
 const myEventsList = [
-    { title: 'Cumpleaños del jefe', start: moment().toDate(), end: moment().add(2, "hours").toDate(), bgcolor: "#fafafa"}
+    { 
+        title: 'Cumpleaños del jefe',
+        start: moment().toDate(),
+        end: moment().add(2, "hours").toDate(),
+        bgcolor: "#fafafa",
+        notes:'Comprar el pastel',
+        user: {
+            _id: '123',
+            name: 'Cheque'
+        }
+    }
 ];
 
 export const CalendarScreen = () => {
 
+    const [lastView, setLastView] = useState(localStorage.getItem("lastView") || 'month');
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    }
+
+    const onSelectEvent = (e) => {
+        console.log(e);
+    };
+
+    const onViewChange = (e) =>{
+        setLastView(e);
+        localStorage.setItem("lastView", e);
+    };
+
     const eventStyleGetter = (event, start, end, isSelected) => {
-        console.log(event, start, end, isSelected);
+        
         const style={
             backgroundColor: '#367CF7',
             borderRadius: '0px',
@@ -40,8 +67,16 @@ export const CalendarScreen = () => {
                 endAccessor="end"
                 messages={messages}
                 eventPropGetter={eventStyleGetter}
-                notes='Comprar el pastel'
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                view={lastView}
+                components={{
+                    event: CalendarEvent
+                }}
             />
+
+            <CalendarModal />
         </div>
     )
 }
